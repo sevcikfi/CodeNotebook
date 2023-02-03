1b710175a4185e15aff33ef186d1e602ed7830b6
Hayaka <fisevcik@email.cz>
2023-02-03 23:00:07 +0100

    dotnet console test - working

---
+++ dotnet/rzikm-CreditExam/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace rzikm_CreditTest;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("おはよう World!");
+        Console.Error.WriteLine("rzkm testing!");
+        String unicodeString =
+            "This Unicode string has 2 characters outside the " +
+            "ASCII range:\n" +
+            "Pi (\u03a0), and Sigma (\u03a3).";
+        Console.WriteLine(unicodeString);
+
+        
+        Console.ReadKey();
+    }
+}
